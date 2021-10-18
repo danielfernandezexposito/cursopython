9e00cce52a2adb6560670ff38515f67706d24dde
Fernández Expósito <dfernandeze@minsait.com>
2021-10-18 15:30:05 +0200

    Suma y resta añadidas

---
+++ int.py
@@ -0,0 +1,8 @@
+# Tipo de dato "int"
+#ejemplo facil suma
+
+a=int(input("Introduzca un numero: "))
+b=int(input("Introduzca otro numero: "))
+
+print(f"La suma de ambos numeros es {a+b}")
+print(f"La resta de ambos numeros es {a-b}")
