f07797d3b8c83a611f07ebf90cea0d3563cef7a4
Fernández Expósito <dfernandeze@minsait.com>
2021-10-19 08:43:18 +0200

    Ejemplo tuplas

---
+++ tuplas.py
@@ -0,0 +1,11 @@
+# Tipo de dato "tuplas"
+#inmutables y ordenadas, permite repeticion
+#se define con "(" , ")" , pueden ser cualquier tipo
+#ejemplo 
+
+
+tupla_entero=(1,3,4,5,3,7,2,4)
+tupla_fruta=("manzana","platano","albaricoque")
+
+print(tupla_entero)
+print(tupla_fruta)
