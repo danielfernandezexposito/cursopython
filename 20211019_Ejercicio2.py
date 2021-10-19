00672ba978d939bcd8b8add0cd6dfccae951c0a5
Fernández Expósito <dfernandeze@minsait.com>
2021-10-19 13:51:33 +0200

    revision

--- 20211019_Ejercicio2.py
+++ 20211019_Ejercicio2.py
@@ -1,25 +1,26 @@
 # Ejercicio  2 19/10/2021
 
-# Modificar el ejercicio herencia.py a las clases Animal y Perro. De manera que Perro tiene un número de patas 
+# Modificar el ejercicio herencia.py
+# a las clases Animal y Perro. De manera que Perro tiene un número de patas
 
 class Animal(object):
-    def __init__(self, especie ):
-        self.especie= especie
-
-
+    def __init__(self, especie):
+        self.especie = especie
 
     def __str__(self):
         return "especie:"+self.especie
 
+
 class Perro(Animal):
-    def __init__(self, especie, patas ):
+    def __init__(self, especie, patas):
         super(Perro, self).__init__(especie)
         self.patas = patas
 
     def __str__(self):
         return "especie:" + self.especie + " patas: " + self.patas
 
+
 a = Animal("perro")
 print(a)
-b = Perro("perro","4")
-print(b) 
+b = Perro("perro", "4")
+print(b)
